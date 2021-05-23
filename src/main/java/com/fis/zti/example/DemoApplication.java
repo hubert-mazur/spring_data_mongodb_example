5fc1d4ae07b074b6d14fa2435982293854602a7c
hubert-mazur <hiusvii@gmail.com>
2021-05-23 16:55:50 +0200

    added required imports

--- src/main/java/com/fis/zti/example/DemoApplication.java
+++ src/main/java/com/fis/zti/example/DemoApplication.java
@@ -1,5 +1,9 @@
 package com.fis.zti.example;
 
+import com.fis.zti.example.examples.example1.Example1;
+import com.fis.zti.example.examples.example2.Example2;
+import com.fis.zti.example.examples.example3.Example3;
+import com.fis.zti.example.examples.example4.Example4;
 import com.fis.zti.example.examples.example5.Example5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,14 +18,12 @@ public class DemoApplication {
         ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
 
 
-//        Example1 example1 = new Example1();
+        Example1 example1 = new Example1();
 //        Example2 example2 = new Example2();
 //        Example3 example3 = new Example3();
 //        Example4 example4 = new Example4();
-
-        Example5 example5 = new Example5(applicationContext);
+//        Example5 example5 = new Example5(applicationContext);
 
     }
-
 
 }
